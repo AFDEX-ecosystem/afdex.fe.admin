f9721cb9bb1a64760f78dafe8ce473d36d71ec42
Your Name <you@example.com>
2024-08-12 00:40:11 +0700

    Login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,39 +1,38 @@
 <template>
   <div class="login_wrap">
+    <img :src="logo" class="w-[220px] mb-[40px]" alt=""/>
     <div class="login_box">
       <div class="login_header">
-        <h3 class="login_logo">
-          <img src="@/assets/images/common/logo.svg" alt="영산대학교"/>
+        <h3 class="login_logo text-4xl flex justify-center font-bold">
+          Đăng nhập AFDEX
         </h3>
       </div>
       <div class="login_input">
-        <strong>아이디</strong>
+        <strong>Số điện thoại</strong>
         <input type="text" v-model="username" class="form_style wd_p100" placeholder="아이디를 입력하세요"/>
       </div>
       <div class="login_input">
-        <strong>비밀번호</strong>
+        <strong>Mật khẩu</strong>
         <input type="password" v-model="password" class="form_style wd_p100" placeholder="비밀번호를 입력하세요"/>
       </div>
       <p class="login_error" v-if="errorMessage">아이디가 일치하지 않습니다.</p>
 
       <div class="login_btn_area">
-        <button type="button" class="btn_round btn_primary btn_xl btn_login" @click="login">로그인</button>
+        <button type="button" class="btn_round bg-[--base-color-1] text-white btn_xl btn_login" @click="login">Đăng nhập</button>
       </div>
 
       <div class="remember_id">
         <div class="check_row">
           <input type="checkbox" name="join_check" id="join_check1" class="hidden"/>
-          <label for="join_check1">아이디 저장</label>
+          <label for="join_check1">Lưu tài khoản</label>
         </div>
       </div>
     </div>
     <p class="login_tip">
-      * ID/PW는 학사정보시스템 접속정보를 이용합니다.<br />
-      * ID/PW 조회 및 로그인 관련문의는 000-0000-0000으로 문의해주세요.
+      <!-- Tổng ài hỗ trợ: 19001900 <br/>
+ Hỗ trợ khách hàng các ngày trong tuần <br/> từ Thứ Hai ến Chủ nhật <br/>
+ (từ 07h00 - 22h hàng ngày) -->
     </p>
-    <footer id="footer" class="footer_wrap">
-      <p class="copyright">Copyright @2024 <strong>Youngsan University.</strong> All Right Reserved</p>
-    </footer>
   </div>
   <LoaddingComponent v-if="isLoading"></LoaddingComponent>
 </template>
@@ -45,6 +44,7 @@ import LoaddingComponent from '@/components/common/loading/LoaddingComponent.vue
 import {setUserInfo} from '@/utils/storage'
 import router from '@/router'
 import {commonStore} from '@/stores/common'
+import logo from '@/assets/images/icon/logo white.png'
 
 export default {
   components: {
@@ -52,8 +52,8 @@ export default {
   },
   data() {
     return {
-      username: 'dainls',
-      password: 'dainls123!@#',
+      username: '032***',
+      password: '032***',
       isLoad: false,
       isSubmitted: false,
       openModal: [
@@ -72,7 +72,8 @@ export default {
     const store = commonStore()
 
     return {
-      store
+      store,
+      logo
     }
   },
   mounted() {
